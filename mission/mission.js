3ef8c0120d65e9e043c3cbc3f0dbb66bd906e5c3
ConKChristen <connork3163@gmail.com>
2025-05-03 15:37:52 -0600

    Prove 2nd week

---
+++ mission/mission.js
@@ -0,0 +1,19 @@
+let selectElem = document.querySelector("select");
+let logo = document.querySelector("img");
+
+selectElem.addEventListener("change", changeTheme);
+
+function changeTheme() {
+    let current_theme = selectElem.value;
+    if (current_theme == "Dark"){
+        document.body.className="dark";
+        logo.src = "byui-logo_white.png";
+        logo.alt = "New dark more image";
+        document.body.appendChild(logo);
+    } else {
+        document.body.className="white";
+        logo.src = "byui-logo_blue.webp";
+        logo.alt = "Light Mode logo";
+        document.body.appendChild(logo);
+    }
+}
